f41650e5fba6c635314f85ebc749c6f46892386d
Rishi Chopra <github.com@rishichopra.org>
2017-12-19 00:10:41 -0800

    Lesson 5: 3. Quiz: Quiz: First Steps With ARIA
    Added the logic for an 'aria-checked' toggle consistent with the framework/skeleton provided...

--- lesson5-semantics-aria/03-first-steps/checkboxes.js
+++ lesson5-semantics-aria/03-first-steps/checkboxes.js
@@ -20,7 +20,9 @@
     this.el.addEventListener('keydown', this.handleKeyDown.bind(this));
     this.el.addEventListener('click', this.toggle.bind(this));
 
-    // Any other set-up we want to do here?
+    this.el.setAttribute('role', "checkbox");    
+    this.el.setAttribute('aria-checked', "false");
+    
   }
 
   Checkbox.prototype.handleKeyDown = function(e) {
@@ -36,14 +38,10 @@
   Checkbox.prototype.toggle = function() {
     if (this.el.hasAttribute('checked')) {
       this.el.removeAttribute('checked');
-
-      // Hmm.
-
+      this.el.setAttribute('aria-checked', "false");
     } else {
       this.el.setAttribute('checked', '');
-
-      // Hmmmmm.
-
+      this.el.setAttribute('aria-checked', "true");      
     }
   };
 
